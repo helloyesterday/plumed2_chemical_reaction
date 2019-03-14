6c4ab177852e9e431d37a8e6351e450b11886b7f
helloyesterday <yesterday.young@gmail.com>
2019-03-14 09:57:40 +0800

    another type of wall

---
+++ src/bias/UWalls2.cpp
@@ -0,0 +1,156 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2012-2018 The plumed team
+   (see the PEOPLE file at the root of the distribution for a list of names)
+
+   See http://www.plumed.org for more information.
+
+   This file is part of plumed, version 2.
+
+   plumed is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   plumed is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#include "Bias.h"
+#include "ActionRegister.h"
+
+
+using namespace std;
+
+
+namespace PLMD {
+namespace bias {
+
+//+PLUMEDOC BIAS UPPER_WALLS
+/*
+Defines a wall for the value of one or more collective variables,
+ which limits the region of the phase space accessible during the simulation.
+
+The restraining potential starts acting on the system when the value of the CV is greater
+(in the case of UPPER_WALLS) or lower (in the case of LOWER_WALLS) than a certain limit \f$a_i\f$ (AT)
+minus an offset \f$o_i\f$ (OFFSET).
+The expression for the bias due to the wall is given by:
+
+\f$
+  \sum_i {k_i*d_i}log(1+exp(-2*(x_i-a_i+o_i)/d_i))
+\f$
+
+\f$k_i\f$ (KAPPA) is an energy constant in internal unit of the code, \f$s_i\f$ (EPS) a rescaling factor and
+\f$e_i\f$ (EXP) the exponent determining the power law. By default: EXP = 2, EPS = 1.0, OFFSET = 0.
+
+
+\par Examples
+
+The following input tells plumed to add both a lower and an upper walls on the distance
+between atoms 3 and 5 and the distance between atoms 2 and 4. The lower and upper limits
+are defined at different values. The strength of the walls is the same for the four cases.
+It also tells plumed to print the energy of the walls.
+\plumedfile
+DISTANCE ATOMS=3,5 LABEL=d1
+DISTANCE ATOMS=2,4 LABEL=d2
+UPPER_WALLS ARG=d1,d2 AT=1.0,1.5 KAPPA=150.0,150.0 EXP=2,2 EPS=1,1 OFFSET=0,0 LABEL=uwall
+LOWER_WALLS ARG=d1,d2 AT=0.0,1.0 KAPPA=150.0,150.0 EXP=2,2 EPS=1,1 OFFSET=0,0 LABEL=lwall
+PRINT ARG=uwall.bias,lwall.bias
+\endplumedfile
+
+*/
+//+ENDPLUMEDOC
+
+class UWalls2 : public Bias {
+  std::vector<double> at;
+  std::vector<double> kappa;
+  std::vector<double> dep;
+  std::vector<double> offset;
+  
+  const double u_lim=154.0*std::log(10.0);
+  const double l_lim=-154.0*std::log(10.0);
+public:
+  explicit UWalls2(const ActionOptions&);
+  void calculate();
+  static void registerKeywords(Keywords& keys);
+};
+
+PLUMED_REGISTER_ACTION(UWalls2,"UPPER_WALLS2")
+
+void UWalls2::registerKeywords(Keywords& keys) {
+  Bias::registerKeywords(keys);
+  keys.use("ARG");
+  keys.add("compulsory","AT","the positions of the wall. The a_i in the expression for a wall.");
+  keys.add("compulsory","KAPPA","the force constant for the wall.  The k_i in the expression for a wall.");
+  keys.add("compulsory","OFFSET","0.0","the offset for the start of the wall.  The o_i in the expression for a wall.");
+  keys.add("compulsory","DEPTH","0.1","the depth of the walls.  The d_i in the expression for a wall.");
+  keys.addOutputComponent("force2","default","the instantaneous value of the squared force due to this bias potential");
+}
+
+UWalls2::UWalls2(const ActionOptions&ao):
+  PLUMED_BIAS_INIT(ao),
+  at(getNumberOfArguments(),0),
+  kappa(getNumberOfArguments(),0.0),
+  dep(getNumberOfArguments(),0.1),
+  offset(getNumberOfArguments(),0.0)
+{
+  // Note : the sizes of these vectors are checked automatically by parseVector
+  parseVector("OFFSET",offset);
+  parseVector("DEPTH",dep);
+  parseVector("KAPPA",kappa);
+  parseVector("AT",at);
+  checkRead();
+
+  log.printf("  at");
+  for(unsigned i=0; i<at.size(); i++) log.printf(" %f",at[i]);
+  log.printf("\n");
+  log.printf("  with an offset");
+  for(unsigned i=0; i<offset.size(); i++) log.printf(" %f",offset[i]);
+  log.printf("\n");
+  log.printf("  with force constant");
+  for(unsigned i=0; i<kappa.size(); i++) log.printf(" %f",kappa[i]);
+  log.printf("\n");
+  log.printf("  and depth");
+  for(unsigned i=0; i<dep.size(); i++) log.printf(" %f",dep[i]);
+  log.printf("\n");
+
+  addComponent("force2"); componentIsNotPeriodic("force2");
+}
+
+void UWalls2::calculate() {
+  double ene = 0.0;
+  double totf2 = 0.0;
+  for(unsigned i=0; i<getNumberOfArguments(); ++i) {
+    double f = 0.0;
+    const double cv=difference(i,at[i],getArgument(i));
+    const double k=kappa[i];
+    const double off=offset[i];
+    const double d0=dep[i];
+    const double cvoff=cv+off;
+    const double uscale = cvoff/d0;
+    if(uscale<l_lim)
+      f = 0;
+	else if(uscale>u_lim)
+	{
+	  f = -k;
+      ene += k * cvoff;
+	}
+	else
+	{
+      const double exp_p2 = std::exp(2*uscale);
+      const double exp_n2 = std::exp(-2*uscale);
+      f = -k / (1.0 + exp_n2);
+      ene += 0.5 * k * d0 * std::log(1.0 + exp_p2);
+    }
+    totf2 += f * f;
+    setOutputForce(i,f);
+  }
+  setBias(ene);
+  getPntrToComponent("force2")->set(totf2);
+}
+
+}
+}
